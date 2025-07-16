a3a09ec168b7d81269cff7c60e919a587e7f2ff7
Vitaly Shcheglov <hallovit@yandex.ru>
2025-07-16 10:25:02 +0500

    modified files

--- config/courses/views.py
+++ config/courses/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 
@@ -13,6 +15,11 @@ class CourseViewSet(viewsets.ModelViewSet):
 class LessonListCreate(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    filterset_fields = ['course']
+    ordering_fields = ['title', 'payment_date']
+    ordering = ['title']
 
 
 class LessonDetail(generics.RetrieveUpdateDestroyAPIView):
