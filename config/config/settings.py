19db2f0b9703559d970ca6a25601dbf1e312ef07
Vitaly Shcheglov <hallovit@yandex.ru>
2025-07-23 11:21:05 +0500

    connection of the possibility of payment for courses

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,6 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-=y$0yiwm=xd4es=alyih^!srfsqgv7)^w7!^iqhu-0c+owl@(*')
 
+STRIPE_TEST_SECRET_KEY = os.getenv('STRIPE_TEST_SECRET_KEY')
+STRIPE_TEST_PUBLIC_KEY = os.getenv('STRIPE_TEST_PUBLIC_KEY')
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
