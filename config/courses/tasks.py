40df09d6e9839aff5fa78579998c42ab4f99384e
Vitaly Shcheglov <hallovit@yandex.ru>
2025-07-27 14:26:46 +0500

    add asynchronous mailing

--- config/courses/tasks.py
+++ config/courses/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 from django.utils import timezone
-from .models import Course
+from .models import Course, Subscription
 from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
 
@@ -31,3 +31,22 @@ def notify_users_about_upcoming_courses():
             )
 
             print(f"Уведомление отправлено пользователю {user.email}: У вас начинаются курсы: {course_titles}.")
+
+@shared_task
+def send_course_update_email(course_title, user_email):
+    """
+    Асинхронная задача для отправки уведомления о обновлении курса.
+
+    Args:
+        course_title (str): Заголовок обновленного курса.
+        user_email (str): Электронная почта пользователя, которому отправляется уведомление.
+    """
+    subject = f'Обновление курса: {course_title}'
+    message = f'Курс "{course_title}" был обновлен. Проверьте обновления на сайте.'
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email='no-reply@example.com',  # Замените на ваш адрес отправителя
+        recipient_list=[user_email],
+        fail_silently=False,
+    )
